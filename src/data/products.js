49a24a9bc2215ea6a5176d286237b6c614794f4c
Ramgopalkolla77 <80104689+Ramgopalkolla77@users.noreply.github.com>
2021-05-24 12:10:03 +0530

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Pulse Oximeter',
+    price: 499.99,
+    currency: '₹',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Vaporizer',
+    price: 249.99,
+    currency: '₹',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Dc 3 Layer Masks',
+    price: 239.99,
+    currency: '₹',
+    image: 'images/03.jpg',
+  },
+  {
+    id: 6,
+    name: 'Health Vit C',
+    price: 119.99,
+    currency: '₹',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 5,
+    name: 'Dettol Hand Wash',
+    price: 599.99,
+    currency: '₹',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Tulasi Drops',
+    price: 149.99,
+    currency: '₹',
+    image: 'images/06.png',
+  }
+];
+
+export default data;
